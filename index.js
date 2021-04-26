e7cbdab51b5d7b26b0eee411ccf3e2f2bff9cd13
jacob <jacobharv00@gmail.com>
2021-04-26 15:01:03 -0600

    intro-to-functions

--- index.js
+++ index.js
@@ -1,1 +1,80 @@
 // Follow along with the examples here
+console.log('Hello, world')
+console.log('Hello, world')
+console.log('Hello, world')
+console.log('Hello, world')
+console.log('Hello, world')
+console.log('Hello, world')
+console.log('Hello, world')
+console.log('Hello, world')
+
+function sayHello() {
+    console.log('Hello!')
+}
+
+sayHello()
+sayHello()
+sayHello()
+sayHello()
+sayHello()
+sayHello()
+
+function sayHelloToIsabel() {
+    console.log('Hello, Isabel!')
+}
+
+function sayHelloToSofia() {
+    console.log('Hello, Sofia!')
+}
+
+function sayHelloToBrendan() {
+    console.log('Hello, Brendan!')
+}
+
+sayHelloToIsabel()
+sayHelloToSofia()
+sayHelloToBrendan()
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}`)
+}
+
+sayHelloTo('Isabel')
+sayHelloTo('Jane')
+sayHelloTo('R2-D2')
+sayHelloTo(1)
+
+function say(greeting, firstName) {
+    console.log('firstName: ', firstName)
+    console.log('greeting: ', greeting)
+    console.log(`${greeting}, ${firstName}!`)
+}
+
+say('Goodbye', 'Julio')
+say('Julio', 'hello')
+
+function add(x, y) {
+    return x + y
+}
+
+console.log(add(1, 2))
+console.log(add(80, 9000))
+
+function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`
+}
+
+console.log(say('Hello', 'Sofia'))
+console.log(say('Howdy', 'partner'))
+
+
+function fun(person) {
+   if (person === 'Jacob') {
+        return `You are learning ${person}`
+    } else {
+        return 'Not cool enough'
+    }
+}
+
+console.log(fun('Jacob'))
+console.log(fun('Henry'))
